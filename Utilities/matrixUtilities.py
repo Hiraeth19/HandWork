2ded1d13396fc90f1d97d031a8bab2cbfe28dba9
notSanil <44319106+notSanil@users.noreply.github.com>
2018-11-07 14:34:01 +0530

    Added the second layer of the network and added the weights and biases

--- Utilities/matrixUtilities.py
+++ Utilities/matrixUtilities.py
@@ -12,3 +12,20 @@ def multiply_matrices(x, y):
 def list_to_matrix(list_):
     new = numpy.array(list_)
     return new
+
+
+# Load wieghts from a file
+def load_weights(file):
+    with open(file, 'r') as weights:
+        weight_list = []
+        for line in weights:
+            weight_list.append(line.rstrip().split(','))
+
+        return weight_list
+
+
+# Convert a list of strings to list of ints
+def convert_string_to_int(list_):
+    for i in range(0, len(list_), 1):
+        for j in range(0, len(list_[i]), 1):
+            list_[i][j] = int(list_[i][j])
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from Utilities.imageUtilities import convert_grayscale, open_image
-from Utilities.matrixUtilities import list_to_matrix
+from Utilities.matrixUtilities import list_to_matrix, load_weights, multiply_matrices, convert_string_to_int
+import numpy
 
 
 # TODO: The entire program needs to be tied together with the weights and biases
 if __name__ == '__main__':
-    image = convert_grayscale(open_image('nine.png'))
-    print(len(image))
-    matrix = list_to_matrix(image)
-    print(matrix)
+    image_map = convert_grayscale(open_image('nine.png'))
+    matrix = list_to_matrix(image_map)
+
+    weights1 = load_weights('weights1.txt')
+    convert_string_to_int(weights1)
+    weights1 = list_to_matrix(weights1)
+
+    layer2 = multiply_matrices(weights1, matrix)
+
+    print(len(layer2))
